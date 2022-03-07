a17bb5d497aaec1f458f2a41de4106efc9f83c19
KrawczowaKris <nukinanada@gmail.com>
2022-03-07 18:13:54 +0500

    module1-task3_2

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { renderTemplate, RenderPosition } from './render.js';
+import { createTripTabsTemplate } from "./view/site-trip-tabs";
+import { createTripSortTemplate } from './view/site-trip-sort.js';
+import { createTripFiltersTemplate } from './view/site-trip-filter.js';
+import { createTripEventsItemTemplate } from  './view/site-point.js';
+import { createAddNewPoint } from './view/site-add-new-point.js';
+
+const TripControlsNavigationElement = document.querySelector('.trip-controls__navigation');
+const TripControlsFiltersElement = document.querySelector('.trip-controls__filters');
+const TripEventsElement = document.querySelector('.trip-events');
+const TripEventsListElement = TripEventsElement.querySelector('.trip-events__list');
+
+renderTemplate(TripEventsElement, createAddNewPoint(), RenderPosition.BEFOREEND)
+renderTemplate(TripControlsNavigationElement, createTripTabsTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(TripControlsFiltersElement, createTripFiltersTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(TripEventsElement, createTripSortTemplate(), RenderPosition.AFTERBEGIN);
+for (let i = 0; i < 3; i++) {
+    renderTemplate(TripEventsListElement, createTripEventsItemTemplate(), RenderPosition.BEFOREEND);
+}
+
